71f22ad77a32a69c37c864ee440ad95955ac52af
lklein2505 <lklein00@fesb.hr>
2020-11-17 13:11:56 +0100

    New files added and line changed for first commit

---
+++ internship-1-homework/homework-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Line changed for first commit!");
+        }
+    }
+}
